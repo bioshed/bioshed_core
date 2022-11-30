bd6f252c06043733aa51ea911507c8723d3bf7ec
Jerry Chen <jerry@Jerrys-MacBook-Air.local>
2022-11-29 22:37:21 -0800

    v0.2.7 bioshed run --list works locally too

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='bioshed',
-    version='0.2.6',
+    version='0.2.7',
     description='BioShed Cloud Bioinformatics Tookit',
     install_requires=[
         'boto3',
